93ce9c24b60b52dd5e35cbb4f1b46f2aab30833a
ftm616 <ftm.mhmdi.006@gmail.com>
2025-12-05 08:47:19 +0330

    Add project files.

---
+++ FactorialProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactorialProgram
+{
+    
+    class Program
+    {
+        static int fact (int n)
+        {
+        int s;
+        if(n ==1)
+            return 1;
+            s=n* fact(n-1);
+            return s;
+    }
+
+    static void Main(string[] args)
+        {
+        int a = int.Parse(Console.ReadLine());
+        int factoril = fact (a);
+            Console.WriteLine(factoril);
+            Console.ReadKey();
+        }
+    }
+}
